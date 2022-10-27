0e800f71d1bcb00a79b2b322c6459205d5b32642
Lucky Ligovi <luckyligovi@gmail.com>
2022-10-27 20:28:30 +0300

    assignment completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    # attr_reader :title
+    # attr_writer :title
+
+    def initialize(title)
+        @title = title
+    end
+    
+    def author 
+        @author
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    
+end
+
 
 
